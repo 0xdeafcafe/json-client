7cc761e0101a76fe327a0d7f43c28be18b5f6f1e
Alex Forbes-Reed <alexreed@outlook.com>
2016-06-28 14:37:34 +0100

    added docs, added support for sync calls, with aggex unwrapping

--- src/JsonClientCore/JsonClient.cs
+++ src/JsonClientCore/JsonClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using JsonClientCore.Exceptions;
@@ -13,32 +15,49 @@ namespace JsonClientCore
 	public class JsonClient
 	{
 		/// <summary>
-		/// 
+		/// Options to be passed into every request.
 		/// </summary>
 		public Options Options { get; private set; }
 
 		/// <summary>
-		/// 
+		/// The base url to be prefixed to every request.
 		/// </summary>
 		public Uri BaseUri { get; private set; }
-		
+
 		/// <summary>
-		/// 
+		/// Initializes a new JsonClient.
 		/// </summary>
-		/// <param name="baseUri"></param>
-		/// <param name="options"></param>
+		/// <param name="baseUri">The base url to be prefixed to every request.</param>
+		/// <param name="options">Options to be passed into every request.</param>
 		public JsonClient(Uri baseUri = null, Options options = null)
 		{
 			BaseUri = baseUri;
 			Options = options ?? new Options();
 		}
 
+		/// <summary>
+		/// Makes an async web request.
+		/// </summary>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
 		public async Task<object> RequestAsync(string method, string path = null,
 			Dictionary<string, string> @params = null, Options options = null)
 		{
 			return await RequestAsync<object, object, object>(method, path, @params, null, options);
 		}
 
+		/// <summary>
+		/// Makes an async web request.
+		/// </summary>
+		/// <typeparam name="TResponse">The type of the response object.</typeparam>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
 		public async Task<TResponse> RequestAsync<TResponse>(string method, string path = null,
 			Dictionary<string, string> @params = null, Options options = null)
 			where TResponse : class, new()
@@ -46,6 +65,16 @@ namespace JsonClientCore
 			return await RequestAsync<TResponse, object, object>(method, path, @params, null, options);
 		}
 
+		/// <summary>
+		/// Makes an async web request.
+		/// </summary>
+		/// <typeparam name="TResponse">The type of the response object.</typeparam>
+		/// <typeparam name="TError">The type of the error object.</typeparam>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
 		public async Task<TResponse> RequestAsync<TResponse, TError>(string method, string path = null,
 			Dictionary<string, string> @params = null, Options options = null)
 			where TResponse : class, new()
@@ -55,14 +84,17 @@ namespace JsonClientCore
 		}
 
 		/// <summary>
-		/// 
+		/// Makes an async web request.
 		/// </summary>
-		/// <param name="method"></param>
-		/// <param name="path"></param>
-		/// <param name="params"></param>
-		/// <param name="body"></param>
-		/// <param name="options"></param>
-		public async Task<TResponse> RequestAsync<TResponse, TRequest, TError>(string method, string path = null, 
+		/// <typeparam name="TResponse">The type of the response object.</typeparam>
+		/// <typeparam name="TRequest">The type of the reques object.</typeparam>
+		/// <typeparam name="TError">The type of the error object.</typeparam>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
+		public async Task<TResponse> RequestAsync<TResponse, TRequest, TError>(string method, string path = null,
 			Dictionary<string, string> @params = null, TRequest body = null, Options options = null)
 			where TResponse : class, new()
 			where TRequest : class, new()
@@ -74,7 +106,7 @@ namespace JsonClientCore
 			if (path == null && BaseUri == null)
 				throw new ArgumentNullException(nameof(path), $"The {path} argument can not be null if the {nameof(BaseUri)} options is null.");
 
-			// If the user didn't specify any options params, initalize them
+			// If the user didn't specify any options params, initialize them
 			@params = @params ?? new Dictionary<string, string>();
 			options = options ?? new Options();
 
@@ -93,12 +125,12 @@ namespace JsonClientCore
 					: BaseUri.Append(path);
 
 				// Set Headers
-				foreach(var header in options.Headers)
+				foreach (var header in options.Headers)
 					httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
 
 				// Check if we're sending a body
-				StringContent bodyContent = body == null 
-					? null 
+				StringContent bodyContent = body == null
+					? null
 					: new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
 
 				// Create request
@@ -130,7 +162,7 @@ namespace JsonClientCore
 				{ }
 
 				// Throw time
-				throw new JsonClientException<TError>("An exception occured while executing the http request.")
+				throw new JsonClientException<TError>("An exception occurred while executing the http request.")
 				{
 					Code = response.ReasonPhrase,
 					StatusCode = response.StatusCode,
@@ -144,5 +176,87 @@ namespace JsonClientCore
 				};
 			}
 		}
+
+		#region Sync Wrapper
+
+		/// <summary>
+		/// Makes an async web request.
+		/// </summary>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
+		public object Request(string method, string path = null,
+			Dictionary<string, string> @params = null, Options options = null)
+		{
+			return Request<object, object, object>(method, path, @params, null, options);
+		}
+
+		/// <summary>
+		/// Makes an async web request.
+		/// </summary>
+		/// <typeparam name="TResponse">The type of the response object.</typeparam>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
+		public TResponse Request<TResponse>(string method, string path = null,
+			Dictionary<string, string> @params = null, Options options = null)
+			where TResponse : class, new()
+		{
+			return Request<TResponse, object, object>(method, path, @params, null, options);
+		}
+
+		/// <summary>
+		/// Makes an async web request.
+		/// </summary>
+		/// <typeparam name="TResponse">The type of the response object.</typeparam>
+		/// <typeparam name="TError">The type of the error object.</typeparam>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
+		public TResponse Request<TResponse, TError>(string method, string path = null,
+			Dictionary<string, string> @params = null, Options options = null)
+			where TResponse : class, new()
+			where TError : class, new()
+		{
+			return Request<TResponse, object, TError>(method, path, @params, null, options);
+		}
+
+		/// <summary>
+		/// Makes an async web request.
+		/// </summary>
+		/// <typeparam name="TResponse">The type of the response object.</typeparam>
+		/// <typeparam name="TRequest">The type of the request object.</typeparam>
+		/// <typeparam name="TError">The type of the error object.</typeparam>
+		/// <param name="method">The HTTP request verb.</param>
+		/// <param name="path">The path to be appended to the base url (can be absolute).</param>
+		/// <param name="params">The query string params to be appended to the path.</param>
+		/// <param name="body">The http body of the request.</param>
+		/// <param name="options">Options to be merged with the base options.</param>
+		public TResponse Request<TResponse, TRequest, TError>(string method, string path = null, 
+			Dictionary<string, string> @params = null, TRequest body = null, Options options = null)
+			where TResponse : class, new()
+			where TRequest : class, new()
+			where TError : class, new()
+		{
+			try
+			{
+				return RequestAsync<TResponse, TRequest, TError>(method, path, @params, body, options).Result;
+			}
+			catch (AggregateException ex)
+			{
+				ExceptionDispatchInfo.Capture(ex.Flatten().InnerExceptions.First()).Throw();
+
+				// Required, but never hit
+				return null;
+			}
+		}
+		
+		#endregion
 	}
 }
